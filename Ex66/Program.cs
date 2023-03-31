bc161ff7b4d322550a651f3f2a81bd2de71ba8d0
Egor Muzykantov <linemail@mail.ru>
2023-03-31 09:04:49 +0300

    Решение задачи 66

---
+++ Ex66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.WriteLine(GetSumNumbersFormMToN(1, 15));
+
+int GetSumNumbersFormMToN(int m, int n)
+{
+
+    if (m <= 0 || n <= 0)
+    {
+        Console.WriteLine("Натуальные числа не могут быть меньше или равняться 0");
+        return -1;
+    }
+
+    if (m == n) return m;
+    
+
+    return m + GetSumNumbersFormMToN(++m, n);
+}
